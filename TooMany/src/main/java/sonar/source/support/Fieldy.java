addb3281f64760af7bab79c5eabe09d1f91f9945
Michal Budzowski <michal.budzowski@sonarsource.com>
2020-07-14 17:50:23 +0200

    add files

---
+++ TooMany/src/main/java/sonar/source/support/Fieldy.java
@@ -0,0 +1,33 @@
+public class Fieldy {
+	
+	int f01;
+	int f02;
+	int f03;
+	int f04;
+	int f05;
+	int f06;
+	int f07;
+	int f08;
+	int f09;
+	int f10;
+	int f11;
+	int f12;
+	int f13;
+	int f14;
+	int f15;
+	int f16;
+	int f17;
+	int f18;
+	int f19;
+	int f20;
+	int f21;
+	int f22;
+	int f23;
+	int f24;
+	int f25;
+	int f26;
+	int f27;
+	int f28;
+	int f29;
+	int f30;
+}
